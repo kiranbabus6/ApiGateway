35df13bca2020e2027fad54aea65813f934e8a6a
Kiran <ksekhara3@gatech.edu>
2022-05-15 01:55:51 +0530

    first

---
+++ src/test/java/com/kiran/ms/ag/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.ms.ag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
